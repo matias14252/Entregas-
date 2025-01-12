bcfe52826dc3a19c7859d73265304961c2cc1ce8
eze <115019274+matias14252@users.noreply.github.com>
2025-01-12 00:52:22 -0300

    Delete ProyectoFinal-Banked1-MonardezMatias2024 directory

--- ProyectoFinal-Banked1-MonardezMatias2024/src/routes/chat.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-const chatRouter = Router()
-
-chatRouter.get('/', (req,res) => {
-    res.render('templates/chat', {js: 'chat.js', css: 'chat.css'})
-})
-
-export default chatRouter
