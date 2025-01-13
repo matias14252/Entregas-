7bbfce7351983d092283c4375bdecd0996721139
eze <115019274+matias14252@users.noreply.github.com>
2025-01-12 21:37:39 -0300

    Delete Proyecto-Final-Backend1-Monardez-Matias directory

--- Proyecto-Final-Backend1-Monardez-Matias/src/utils/jwt.js
+++
@@ -1,7 +0,0 @@
-import 'dotenv/config';
-import jwt from 'jsonwebtoken';
-
-export const generateToken = (user) => {
-    const token = jwt.sign({ user }, process.env.JWT_SECRET, { expiresIn: '12h' });
-    return token;
-}
